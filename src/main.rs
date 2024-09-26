e119c25767c443b7b55ee97dc2a0414da0d08675
laz <lazgreen32@gmail.com>
2024-09-26 12:42:58 -0400

    added max map size for memory safety

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{
     collections::HashMap,
     env::{args, set_var},
     fs::read,
-    io::{stdin, BufRead},
+    io::{stdin, stdout, BufRead, Write},
     time::Duration,
     usize,
 };
@@ -21,13 +21,17 @@ fn main() {
             "
 detectif v0.0.1
 
-detectif [optiont] [path] [window cap] [output.csv]
+detectif [optiont] [path] [window cap] [output.csv] [map size (if flag is specified)]
 
 Example: detecif -f path/to/file 18 output.csv
+Example: detecif -si 10 output.csv
+Example: detecif -sim 10 output.csv 100 // this would set the maximum map size to 100. 
 
 Options:
     -f: single file
     -si: standard in
+    -m: used to set maximum size of map in order to prevent out of memory condition. Can only be used with the -si flag
+        default map size is 10_000 which comes out to ~1gb 
 
 Goes through a sequence and finds patterns. Made to find motifs in a nucleotide sequence. Starts at a base length of 3 nucleotides and increases the size of the window until it reaches a user defined cap. 
 
@@ -39,27 +43,36 @@ In the genome pipeline it can take the sequences as a standard input. Using moku
         );
         std::process::exit(3);
     }
+
     let mut window_cap = String::new();
     let mut path = String::new();
-    if in_opt == "-si" {
+    if in_opt.contains("si") {
         let mut input: Vec<String> = Vec::new();
         let mut ids: Vec<String> = Vec::new();
         let std_input = stdin().lines();
         for line in std_input {
             let line = line.unwrap();
             if line.starts_with(">") {
-                if line.len() == 1 {
-                    continue;
-                }
                 ids.push(line[1..].to_string());
             } else {
                 input.push(line);
             }
         }
+
         window_cap.push_str(&args().nth(2).expect("Enter output Maximum motif length"));
         path.push_str(&args().nth(3).expect("Enter output file name"));
+        let mut map_max = 0;
+        if in_opt.contains("m") {
+            let map_max_arg = args().nth(4).expect("Enter Maximum map size");
+            let map_max_parsed = map_max_arg.parse::<i32>().unwrap();
+            map_max += map_max_parsed;
+        } else {
+            map_max += 10_000;
+        }
         let mut count = 0;
         let total = input.clone();
+        let mut map_of_maps: HashMap<String, HashMap<String, u32>> = HashMap::new();
+        let mut mem_clear_counter = 0;
         for id in input {
             let map = search(
                 &id,
@@ -68,13 +81,29 @@ In the genome pipeline it can take the sequences as a standard input. Using moku
                 (total.len()) as i32,
                 (count + 1) as i32,
             );
-            let numbered_path = format!("{}{}.csv", path, &ids[count]);
-            write_csv(&map, &numbered_path);
+            write_csv(&map, &format!("{}{}.csv", path, &ids[count]));
+            if map_of_maps.len() < map_max as usize {
+                map_of_maps.insert(ids[count].clone(), map.clone());
+            } else {
+                stdout().write_all("Map Clear\n".as_bytes()).unwrap();
+                mem_clear_counter += 1;
+                write_big_csv(
+                    &map_of_maps,
+                    &format!("{}_all{}.csv", path, mem_clear_counter),
+                );
+                map_of_maps.clear();
+            }
             count += 1;
+            // keep this at the end of the loop or it will throw a IOOB
         }
+        mem_clear_counter += 1;
+        write_big_csv(
+            &map_of_maps,
+            &format!("{}_all{}.csv", path, mem_clear_counter),
+        );
     }
 
-    if in_opt == "-f" {
+    if in_opt.contains("-f") {
         let mut input = String::new();
         input.push_str(&args().nth(2).expect("Enter a file to check"));
         window_cap.push_str(&args().nth(3).expect("Enter output Maximum motif length"));
@@ -100,6 +129,24 @@ In the genome pipeline it can take the sequences as a standard input. Using moku
             1,
         );
         write_csv(&map, &path);
+    }
+}
+
+fn write_big_csv(hashmap: &HashMap<String, HashMap<String, u32>>, path: &str) {
+    let mut writer = Writer::from_path(path).expect("Could not open file");
+    writer
+        .write_record(Vec::from(["id", "pattern", "count"]))
+        .unwrap();
+    for key_id in hashmap.keys() {
+        let current_pair1 = hashmap.get_key_value(key_id).unwrap();
+        let id = current_pair1.0;
+        for key_record in current_pair1.1 {
+            let mut current_record: Vec<String> = Vec::new();
+            current_record.push(id.to_string());
+            current_record.push(key_record.0.to_string());
+            current_record.push(key_record.1.to_string());
+            writer.write_record(current_record).unwrap();
+        }
     }
 }
 
